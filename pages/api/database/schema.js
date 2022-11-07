3827b87d226e131651e97592e50888725ade7b8b
frankbejo <frankyenbejo@gmail.com>
2022-11-07 17:37:59 +0800

    new commit

--- pages/api/database/schema.js
+++ pages/api/database/schema.js
@@ -16,5 +16,5 @@ const postsSchema = new mongoose.Schema({
     }
 }, {timestamps: true});
 
-const posts = mongoose.model("Posts",postsSchema)
+const posts = mongoose.model("Posts")||mongoose.model("Posts", postsSchema)
 export default posts;
