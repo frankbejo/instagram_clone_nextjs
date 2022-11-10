c1532c2944dcf9398fbce5f830ae5f84a6514f70
frankbejo <frankyenbejo@gmail.com>
2022-11-11 00:53:03 +0800

    new commit

--- pages/components/Newsfeed.js
+++ pages/components/Newsfeed.js
@@ -1,6 +1,7 @@
 import styles from '../../styles/Home.module.css'
+import {MdMoreHoriz, MdBookmarkBorder} from 'react-icons/md'
+import {AiOutlineHeart, AiOutlineComment, AiOutlineSend} from 'react-icons/ai'
 
-const game = []
 function Newsfeed({posts, users}){
     return(
         <div className={styles.newsfeed}>
@@ -15,10 +16,71 @@ function Newsfeed({posts, users}){
                             {
                                 users.filter(user => post.post_owner_id === user._id
                                 ).map(item => {
-                                    return item._id
+                                    return <div className={styles.post}>
+                                        <div className={styles.post_header}>
+                                            <div className={styles.post_header_left}>
+                                                <img src={item.image} alt="" />
+                                                <div className={styles.post_owner_info}>
+                                                    <span>{post.post_owner_name}</span>
+                                                    <br />
+                                                    {
+                                                        post.post_location_name ? (
+                                                            <span>{post.post_location_name}</span>
+                                                        ):
+                                                        (null)
+                                                    }
+                                                </div>
+                                            </div>
+                                            
+                                            <div className={styles.post_setting}>
+                                                <MdMoreHoriz />
+                                            </div>
+                                        </div>
+                                        <div className={styles.content_container}>
+                                                {
+                                                    post.content ? (
+                                                        <img src={post.content[0]} type="image/jpg" width="100%" height="100%" />
+                                                    ):
+                                                    (null)
+                                                }
+                                        </div>
+                                        <div className={styles.post_footer}>
+                                                <div className={styles.post_menus}>
+                                                    <div className={styles.post_menus_left}>
+                                                        <AiOutlineHeart />
+                                                        <AiOutlineComment />
+                                                        <AiOutlineSend />
+                                                    </div>
+                                                    <div className={styles.bookmark}>
+                                                        <MdBookmarkBorder />
+                                                    </div>
+                                                </div>
+                                                <span className={styles.likes}>
+                                                    200,000 likes
+                                                </span>
+                                                
+                                                <div className={styles.caption}>
+                                                    <span>{post.post_owner_name} </span>
+                                                    {
+                                                        post.post ? 
+                                                        post.post :
+                                                        null
+                                                    }
+                                                </div>
+                                                <div className={styles.comments}>
+                                                    View all 1,818 comments
+                                                </div>
+                                                <span className={styles.timespan}>
+                                                    5 HOURS AGO
+                                                </span>
+                                                <div className={styles.add_comment}>
+
+                                                </div>
+                                        </div>
+                                    </div>
+                                    
                                 })
                             }
-                            {post.post_owner_name}
                             </li>
                     }):
                     null
